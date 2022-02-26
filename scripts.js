86b3246e41b839b9bb7aeb649702e0aed214f364
Ted Crisan <tedcrisan@live.ca>
2022-02-25 20:54:19 -0500

    initial setup

---
+++ scripts.js
@@ -0,0 +1,33 @@
+const button = document.getElementById("runaway-btn");
+
+const animateMove = (element, prop, pixels) =>
+  anime({
+    targets: element,
+    [prop]: `${pixels}px`,
+    easing: "easeOutCirc",
+  });
+
+["mouseover", "click"].forEach(function (el) {
+  button.addEventListener(el, function (event) {
+    const top = getRandomNumber(window.innerHeight - this.offsetHeight);
+    const left = getRandomNumber(window.innerWidth - this.offsetWidth);
+
+    animateMove(this, "left", left).play();
+    animateMove(this, "top", top).play();
+  });
+});
+
+const getRandomNumber = (num) => {
+  return Math.floor(Math.random() * (num + 1));
+};
+
+let touches = 0;
+let mainButton = document.getElementById("runaway-btn");
+let exitButton = document.getElementById("exit");
+mainButton.addEventListener("mouseover", (e) => {
+  touches++;
+  console.log(touches);
+  if (touches >= 5) {
+    exitButton.style.display = "inline";
+  }
+});
