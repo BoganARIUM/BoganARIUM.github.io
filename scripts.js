23e379c88a19e79c88fbc16d199babf2ae1f79ae
Ted Crisan <tedcrisan@live.ca>
2022-03-05 16:35:43 -0500

    working video with code

--- scripts.js
+++ scripts.js
@@ -1,8 +1,12 @@
 let showPlaceholder = true;
 let touches = 0;
+let userInput = "";
+let valid = "ArrowUpArrowUpArrowDownArrowDownArrowLeftArrowRightArrowLeftArrowRightba";
 
-const button = document.getElementById("runaway-btn");
-let errorScreen = document.getElementById("error");
+const runaway = document.getElementById("runaway-btn");
+const errorScreen = document.getElementById("error");
+const boganVideo = document.getElementById("bogan-video");
+const video = document.getElementById("video");
 
 const animateMove = (element, prop, pixels) =>
   anime({
@@ -12,7 +16,7 @@ const animateMove = (element, prop, pixels) =>
   });
 
 ["mouseover", "click"].forEach(function (el) {
-  button.addEventListener(el, function (event) {
+  runaway.addEventListener(el, function (event) {
     const top = getRandomNumber(window.innerHeight - this.offsetHeight);
     const left = getRandomNumber(window.innerWidth - this.offsetWidth);
 
@@ -21,7 +25,7 @@ const animateMove = (element, prop, pixels) =>
 
     touches++;
     if (touches >= 5) {
-      errorScreen.style.zIndex = "10";
+      errorScreen.style.zIndex = "2";
       errorScreen.style.opacity = "1";
     }
   });
@@ -30,3 +34,17 @@ const animateMove = (element, prop, pixels) =>
 const getRandomNumber = (num) => {
   return Math.floor(Math.random() * (num + 1));
 };
+
+document.addEventListener("keydown", function (event) {
+  userInput = userInput.concat(event.key);
+  if (userInput === valid) {
+    boganVideo.style.zIndex = "3";
+    boganVideo.style.opacity = "1";
+    video.play();
+    setTimeout(() => {
+      userInput = "";
+      boganVideo.style.zIndex = "-2";
+      boganVideo.style.opacity = "0";
+    }, 31000);
+  }
+});
